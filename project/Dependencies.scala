535fc8ea96cb6a2d6a4d49e9e8230bd7f578312e
Azat Khamzin <AzRKhamzin@gmail.com>
2023-07-04 13:32:43 +0300

    add: tofu-kernel dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,9 +5,11 @@ object Dependencies {
     lazy val catsEffect = "3.5.1"
     lazy val catsCore = "2.9.0"
     lazy val tsec = "0.4.0"
+    lazy val tofu = "0.12.0.1"
   }
 
   def tsec(artifact: String): ModuleID = "io.github.jmcardon" %% artifact % V.tsec
+  def tofu(artifact: String): ModuleID = "tf.tofu" %% artifact % V.tofu
 
   val catsEffect = "org.typelevel" %% "cats-effect" % V.catsEffect
   val catsCore = "org.typelevel" %% "cats-core" % V.catsCore
@@ -15,6 +17,7 @@ object Dependencies {
   val allDeps: Seq[sbt.ModuleID] = Seq(
     catsEffect,
     catsCore,
+    tofu("tofu-kernel"),
     tsec("tsec-common"),
     tsec("tsec-password"),
     tsec("tsec-mac"),
