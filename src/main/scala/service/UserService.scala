4b70fe43adf5d173da16772a8428a4d80f60e289
Azat Khamzin <AzRKhamzin@gmail.com>
2023-07-04 11:10:58 +0300

    add user service

---
+++ src/main/scala/service/UserService.scala
@@ -0,0 +1,45 @@
+package service
+
+import cats.data.EitherT
+import cats.implicits.toFunctorOps
+import cats.{Functor, Monad}
+import domain.users.errors.{LoginInUse, UserError, UserNotFound}
+import domain.users.{User, UserRepositoryAlgebra}
+
+import java.util.UUID
+
+class UserService[F[_]](userRepo: UserRepositoryAlgebra[F]) {
+  def createUser(user: User)(implicit M: Monad[F]): EitherT[F, LoginInUse, User] =
+    for {
+      saved <- userRepo.create(user)
+    } yield saved
+
+  def getUser(userId: UUID)(implicit F: Functor[F]): EitherT[F, UserNotFound, User] =
+    userRepo.get(userId).toRight(UserNotFound)
+
+  def getUserByName(
+    userName: String,
+  )(implicit F: Functor[F]): EitherT[F, UserNotFound, User] =
+    userRepo.findByUserName(userName).toRight(UserNotFound)
+
+  def deleteUser(userId: UUID)(implicit F: Functor[F]): F[Unit] =
+    userRepo.delete(userId).value.void
+
+  def deleteByUserName(userName: String)(implicit F: Functor[F]): F[Unit] =
+    userRepo.deleteByUserName(userName).value.void
+
+  def update(user: User)(implicit M: Monad[F]): EitherT[F, UserError, User] =
+    for {
+      saved <- userRepo.update(user)
+    } yield saved
+
+  def list: F[List[User]] =
+    userRepo.list
+}
+
+object UserService {
+  def apply[F[_]](
+    repository: UserRepositoryAlgebra[F],
+  ): UserService[F] =
+    new UserService[F](repository)
+}
