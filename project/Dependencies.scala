b542e3572a22f27eb65443bb3ada315e6fb53a5a
Azat Khamzin <AzRKhamzin@gmail.com>
2023-07-06 12:38:40 +0300

    add tapir dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,9 +9,11 @@ object Dependencies {
     lazy val newtype = "0.4.4"
     lazy val circe = "0.14.5"
     lazy val jwt = "0.9.1"
+    lazy val tapir = "1.4.0"
   }
 
   def tofu(artifact: String): ModuleID = "tf.tofu" %% artifact % V.tofu
+  def tapir(artifact: String): ModuleID = "com.softwaremill.sttp.tapir" %% s"tapir-$artifact" % V.tapir
 
   val catsEffect = "org.typelevel" %% "cats-effect" % V.catsEffect
   val catsCore = "org.typelevel" %% "cats-core" % V.catsCore
@@ -33,5 +35,8 @@ object Dependencies {
     tofu("tofu-logging-structured"),
     tofu("tofu-core-ce3"),
     tofu("tofu-doobie-logging-ce3"),
+    tapir("core"),
+    tapir("http4s-server"),
+    tapir("json-circe")
   )
 }
