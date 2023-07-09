947c13afef8d63b8f78965d5e2fa8228447e661c
Azat Khamzin <AzRKhamzin@gmail.com>
2023-07-09 12:51:35 +0300

    refactor Role

--- src/main/scala/domain/users/Role.scala
+++ src/main/scala/domain/users/Role.scala
@@ -5,8 +5,8 @@ import cats.Eq
 final case class Role(roleRepr: String)
 
 object Role {
-  val Admin: Role = Role("Admin")
-  val Customer: Role = Role("Customer")
+  val Admin: Role = Role("admin")
+  val Customer: Role = Role("customer")
 
   implicit val eqRole: Eq[Role] = Eq.fromUniversalEquals[Role]
 }
